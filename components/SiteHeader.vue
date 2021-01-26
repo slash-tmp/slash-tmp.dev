4d380768d8074280df670371cc57150002ec471a
Adrien Boutigny <adrien.boutigny@gmail.com>
2021-01-27 00:53:09 +0100

    fix: adjust header layout

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -1,6 +1,8 @@
 <template>
   <header class="site-header">
-    <div class="site-header__company-name">/tmp</div>
+    <div class="site-header__content container container--medium">
+      <nuxt-link class="site-header__company-name" to="/">/tmp</nuxt-link>
+    </div>
   </header>
 </template>
 
@@ -14,14 +16,30 @@ export default {
 @import '@/assets/scss/variables';
 
 .site-header {
+  --outline-color: #{$color-white};
+  --header-font-size: 2.5rem;
+
   background-color: $color-purple;
-  padding: 2.5rem 5rem;
+
+  &__content {
+    @media (max-width: 1200px) {
+      margin: 0 auto;
+    }
+  }
 
   &__company-name {
-    font-size: 3rem;
+    font-size: var(--header-font-size);
     color: $color-white;
     font-family: $font-family-heading;
     font-weight: bold;
+  }
+
+  @media (max-width: 900px) {
+    --header-font-size: 1.5rem;
+  }
+
+  @media (max-width: 600px) {
+    --header-font-size: 1.2rem;
   }
 }
 </style>
