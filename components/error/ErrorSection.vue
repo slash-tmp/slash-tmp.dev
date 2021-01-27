ff1c9081a5a8314834b2d742544e032228992307
Quentin Bellanger <hello@quentin-bellanger.com>
2021-01-27 13:29:45 +0100

    feat: add error page and main component

---
+++ components/error/ErrorSection.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="error container container--small flow">
+    <p>
+      La page que vous cherchez n’existe pas ou a été supprimée (code d'erreur
+      <code>{{ code }}</code
+      >). Voici le lien de la <nuxt-link to="/">page d'accueil</nuxt-link> pour
+      vous guider.
+    </p>
+
+    <p>
+      Si vous ne trouvez toujours pas le contenu que vous cherchez, n’hésitez
+      pas à nous écrire :
+      <a href="mailto:contact@slash-tmp.dev">contact@slash-tmp.dev</a>.
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ErrorSection',
+  props: {
+    code: Number
+  }
+}
+</script>
+
+<style lang="scss">
+.error {
+  margin: 0 auto;
+}
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <article>
+    <Hero tag="header">
+      <h1>Page introuvable</h1>
+    </Hero>
+
+    <ErrorSection :code="error.statusCode" />
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Error',
+  props: {
+    error: Object
+  }
+}
+</script>
