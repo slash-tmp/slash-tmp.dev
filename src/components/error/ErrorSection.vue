8160475512aaa91f4dcff60d9b299df192329f47
Quentin Bellanger <hello@quentin-bellanger.com>
2021-01-28 21:58:30 +0100

    feat: add error page and main component (#20)
    * feat: add error page and main component
    
    * fix: clean merge folder confusion
    
    * refactor: set default props
    
    * feat: handle generic errors

---
+++ src/components/error/ErrorSection.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="error container container--small flow">
+    <p>
+      <template v-if="code === 404">
+        La page que vous cherchez n’existe pas ou a été supprimée
+      </template>
+      <template v-else>
+        Une erreur est survenue dans le traitement de la page
+      </template>
+      (code d'erreur <code>{{ code }}</code
+      >). Voici le lien de la <nuxt-link to="/">page d'accueil</nuxt-link> pour
+      retrouver votre chemin.
+    </p>
+
+    <p>
+      Si vous ne trouvez pas le contenu que vous cherchez, n’hésitez pas à nous
+      écrire :
+      <a href="mailto:contact@slash-tmp.dev">contact@slash-tmp.dev</a>.
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ErrorSection',
+  props: {
+    code: {
+      type: Number,
+      default: 404
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.error {
+  margin: 0 auto;
+}
+</style>
---
+++ src/layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <article>
+    <Hero tag="header">
+      <h1>{{ error.statusCode === 404 ? 'Page introuvable' : 'Erreur' }}</h1>
+    </Hero>
+
+    <ErrorSection :code="error.statusCode" />
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Error',
+  props: {
+    error: {
+      type: Object,
+      default: () => {
+        return { statusCode: 404 }
+      }
+    }
+  }
+}
+</script>
