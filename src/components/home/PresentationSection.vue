7eb61acf8c540ce89074bdda6409137d5c2b1aee
Adrien Boutigny <adrien.boutigny@gmail.com>
2021-01-31 13:01:23 +0100

    feat: use picture elements to serve webp images

--- src/components/home/PresentationSection.vue
+++ src/components/home/PresentationSection.vue
@@ -14,16 +14,41 @@
       qualité web.
     </p>
     <div class="presentation-section__avatars">
-      <img
-        class="presentation-section__avatar"
-        :src="require('@/assets/img/adrien.jpg?resize&size=200')"
-        alt="Adrien"
-      />
-      <img
-        class="presentation-section__avatar"
-        :src="require('@/assets/img/quentin.jpg?resize&size=200')"
-        alt="Quentin"
-      />
+      <picture>
+        <source
+          :srcSet="
+            require('@/assets/img/adrien.jpg?resize&size=200&format=webp')
+          "
+          type="image/webp"
+        />
+        <source
+          :srcSet="require('@/assets/img/adrien.jpg?resize&size=200')"
+          type="image/jpeg"
+        />
+        <img
+          class="presentation-section__avatar"
+          :src="require('@/assets/img/adrien.jpg?resize&size=200')"
+          alt="Adrien"
+        />
+      </picture>
+
+      <picture>
+        <source
+          :srcSet="
+            require('@/assets/img/quentin.jpg?resize&size=200&format=webp')
+          "
+          type="image/webp"
+        />
+        <source
+          :srcSet="require('@/assets/img/quentin.jpg?resize&size=200')"
+          type="image/jpeg"
+        />
+        <img
+          class="presentation-section__avatar"
+          :src="require('@/assets/img/quentin.jpg?resize&size=200')"
+          alt="Quentin"
+        />
+      </picture>
     </div>
   </div>
 </template>
