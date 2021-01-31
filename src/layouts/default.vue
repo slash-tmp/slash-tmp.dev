94d10e984d8affb72fda3b1c074ae47866833dbc
Adrien Boutigny <adrien.boutigny@gmail.com>
2021-01-31 10:25:16 +0100

    refactor: move toaster logic from layout to a mixin

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -13,39 +13,10 @@
 </template>
 
 <script>
+import ToastProviderMixin from '@/mixins/toast-provider'
+
 export default {
   name: 'DefaultLayout',
-  provide() {
-    return {
-      notifications: this.notifications,
-      notify: this.notify,
-      dismissNotification: this.dismissNotification
-    }
-  },
-  data: () => ({
-    nextNotificationId: 0,
-    notifications: [],
-    automaticDismissTimeoutId: null
-  }),
-  methods: {
-    notify(text) {
-      clearTimeout(this.automaticDismissTimeoutId)
-
-      this.dismissNotification()
-      this.notifications.push({
-        id: this.nextNotificationId,
-        text
-      })
-      this.nextNotificationId++
-
-      this.automaticDismissTimeoutId = setTimeout(
-        this.dismissNotification,
-        8000
-      )
-    },
-    dismissNotification() {
-      this.notifications.splice(0, this.notifications.length)
-    }
-  }
+  mixins: [ToastProviderMixin]
 }
 </script>
