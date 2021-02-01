f8b4a283681310d7cc87d9cf47339d9c1553e6c9
Quentin Bellanger <hello@quentin-bellanger.com>
2021-02-01 13:00:36 +0100

    feat: add meta tags (#24)
    * feat: add global meta tags
    
    * feat: create helper function to generate page meta tags
    
    * fix: import missing function in index
    
    * feat: set base url as env variable
    
    * refactor: call netlify environment variable
    
    Co-authored-by: Adrien Boutigny <adrien.boutigny@gmail.com>
    
    Co-authored-by: Adrien Boutigny <adrien.boutigny@gmail.com>

--- src/pages/mentions-legales.vue
+++ src/pages/mentions-legales.vue
@@ -1,14 +1,26 @@
 <template>
   <article>
     <Hero tag="header">
-      <h1>Mentions légales</h1>
+      <h1>{{ title }}</h1>
     </Hero>
     <LegalSection />
   </article>
 </template>
 
 <script>
+import setPageMeta from '@/helpers/setPageMeta'
+
 export default {
-  name: 'Legal'
+  name: 'Legal',
+  data() {
+    return {
+      title: 'Mentions légales',
+      description:
+        "Informations concernant l'identité et les coordonnées de /tmp ainsi que celles de l'hébergeur du site."
+    }
+  },
+  head() {
+    return setPageMeta(this.title, this.description, this.$route.path)
+  }
 }
 </script>
