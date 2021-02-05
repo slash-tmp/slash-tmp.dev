7d494c5c2e650a45ee4c47c10d8c374da8d0c8ce
Quentin Bellanger <hello@quentin-bellanger.com>
2021-02-06 00:42:48 +0100

    feat: animate hero names and pictures

--- src/components/home/PresentationSection.vue
+++ src/components/home/PresentationSection.vue
@@ -5,53 +5,120 @@
       Dévelop&shy;pement et qualité&nbsp;web
     </h1>
     <p>
-      Bonjour ! Nous c’est Adrien et Quentin. On est 2 développeurs web très
-      attachés à la qualité web et à l’expérience utilisateur.
+      Bonjour ! Nous c’est
+      <span
+        :data-highlight="`Là${'\xa0'}c'est${'\xa0'}Adrien`"
+        :class="[
+          'presentation-section__name',
+          {
+            'presentation-section__name--highlighted':
+              highlightedName === 'adrien'
+          }
+        ]"
+        >Adrien</span
+      >
+      et
+      <span
+        :data-highlight="`Et${'\xa0'}la${'\xa0'}Quentin`"
+        :class="[
+          'presentation-section__name',
+          {
+            'presentation-section__name--highlighted':
+              highlightedName === 'quentin'
+          }
+        ]"
+        >Quentin</span
+      >. On est 2 développeurs web très attachés à la qualité web et à
+      l’expérience utilisateur.
     </p>
     <p>
       Et c’est pour ça qu’on a créé “/tmp” (ça se prononce “slash tmp”) : un
       petit studio de développement où on code des sites et on fait de la
       qualité web.
     </p>
-    <div class="presentation-section__avatars">
-      <picture>
-        <source
-          srcset="@/assets/img/adrien.jpg?resize&size=200&format=webp"
-          type="image/webp"
-        />
-        <source
-          srcset="@/assets/img/adrien.jpg?resize&size=200"
-          type="image/jpeg"
-        />
-        <img
-          class="presentation-section__avatar"
-          src="@/assets/img/adrien.jpg?resize&size=200"
-          alt="Adrien"
-        />
-      </picture>
 
-      <picture>
-        <source
-          srcset="@/assets/img/quentin.jpg?resize&size=200&format=webp"
-          type="image/webp"
-        />
-        <source
-          srcset="@/assets/img/quentin.jpg?resize&size=200"
-          type="image/jpeg"
-        />
-        <img
-          class="presentation-section__avatar"
-          src="@/assets/img/quentin.jpg?resize&size=200"
-          alt="Quentin"
-        />
-      </picture>
+    <div class="presentation-section__avatars">
+      <button
+        :class="[
+          'presentation-section__button',
+          {
+            'presentation-section__button--highlighted':
+              highlightedName === 'adrien'
+          }
+        ]"
+        @click="highlightName('adrien')"
+      >
+        <picture>
+          <source
+            srcset="@/assets/img/adrien.jpg?resize&size=200&format=webp"
+            type="image/webp"
+          />
+          <source
+            srcset="@/assets/img/adrien.jpg?resize&size=200"
+            type="image/jpeg"
+          />
+          <img
+            class="presentation-section__avatar"
+            src="@/assets/img/adrien.jpg?resize&size=200"
+            alt="Adrien"
+          />
+        </picture>
+      </button>
+
+      <button
+        :class="[
+          'presentation-section__button',
+          {
+            'presentation-section__button--highlighted':
+              highlightedName === 'quentin'
+          }
+        ]"
+        @click="highlightName('quentin')"
+      >
+        <picture>
+          <source
+            srcset="@/assets/img/quentin.jpg?resize&size=200&format=webp"
+            type="image/webp"
+          />
+          <source
+            srcset="@/assets/img/quentin.jpg?resize&size=200"
+            type="image/jpeg"
+          />
+          <img
+            class="presentation-section__avatar"
+            src="@/assets/img/quentin.jpg?resize&size=200"
+            alt="Quentin"
+          />
+        </picture>
+      </button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'PresentationSection'
+  name: 'PresentationSection',
+  data() {
+    return {
+      highlightedName: null,
+      highlightTimeOutId: null
+    }
+  },
+  methods: {
+    highlightName(name) {
+      clearTimeout(this.highlightTimeOutId)
+
+      if (this.highlightedName === name) {
+        this.highlightedName = null
+        return
+      }
+      this.highlightedName = name
+
+      this.highlightTimeOutId = setTimeout(() => {
+        this.highlightedName = null
+      }, 4000)
+    }
+  }
 }
 </script>
 
@@ -64,6 +131,7 @@ export default {
   --avatar-size: 10rem;
   --avatar-offset: 1rem;
   --avatar-border-width: 0.5rem;
+  --outline-color: #{$color-white};
 
   &__title {
     color: inherit;
@@ -73,25 +141,68 @@ export default {
     margin: 0 auto;
   }
 
+  &__name {
+    position: relative;
+
+    &::after {
+      background: $color-white;
+      border-radius: 0.25rem;
+      color: $color-purple;
+      content: attr(data-highlight);
+      left: 50%;
+      opacity: 0;
+      padding: 0.1rem 0.4rem;
+      position: absolute;
+      top: 50%;
+      transform: translate(-50%, -50%);
+      transition: opacity 0.4s, transform 0.4s;
+      z-index: 1;
+    }
+
+    &--highlighted {
+      &::after {
+        opacity: 1;
+        transform: translate(-50%, -50%) scale(1.2) rotate(-10deg);
+      }
+
+      &:nth-child(2)::after {
+        transform: translate(-50%, -50%) scale(1.2) rotate(10deg);
+      }
+    }
+  }
+
   &__avatars {
     display: flex;
     justify-content: space-evenly;
     padding: 1rem 0;
+  }
 
-    > :nth-child(1) {
-      transform: translateY(calc(var(--avatar-offset)));
+  &__button {
+    border: var(--avatar-border-width) solid $color-white;
+    border-radius: 50%;
+    height: var(--avatar-size);
+    overflow: hidden;
+    padding: 0;
+    transform: translateY(var(--avatar-offset));
+    transition: transform 0.2s;
+    width: var(--avatar-size);
+
+    &:nth-child(2) {
+      transform: translateY(calc(var(--avatar-offset) * -1));
     }
 
-    > :nth-child(2) {
-      transform: translateY(calc(var(--avatar-offset) * -1));
+    &--highlighted {
+      transform: translateY(var(--avatar-offset)) scale(1.2) rotate(10deg);
+
+      &:nth-child(2) {
+        transform: translateY(calc(var(--avatar-offset) * -1)) scale(1.2)
+          rotate(10deg);
+      }
     }
   }
 
   &__avatar {
-    border-radius: 50%;
-    border: var(--avatar-border-width) solid $color-white;
-    width: var(--avatar-size);
-    height: var(--avatar-size);
+    width: 100%;
   }
 
   @media (max-width: 1200px) {
