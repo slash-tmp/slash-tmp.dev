37aab76570cd663c5cf0358f7f94616bb37780d5
Adrien Boutigny <adrien.boutigny@gmail.com>
2021-02-09 08:08:53 +0100

    feat: add skip link to main content

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,8 +1,15 @@
 <template>
   <div tabindex="-1">
+    <router-link
+      class="skip-link"
+      to="#main-content"
+      @click.native="scrollFix('#main-content')"
+    >
+      Aller au contenu principal
+    </router-link>
     <SiteHeader />
 
-    <main>
+    <main id="main-content">
       <Nuxt />
     </main>
 
@@ -23,6 +30,26 @@ export default {
     $route() {
       this.$el.focus()
     }
+  },
+  methods: {
+    // in some cases, the page doesnt scroll to the content, this fixes it
+    scrollFix(hash) {
+      window.location.hash = hash
+    }
   }
 }
 </script>
+
+<style lang="scss">
+.skip-link {
+  position: fixed;
+  margin: 1rem;
+  padding: 1rem;
+  background-color: $color-white;
+  opacity: 0;
+
+  &:focus {
+    opacity: 1;
+  }
+}
+</style>
