27add2a9dc9dfe5e3c3b27b8a856e69fa20c05d6
Adrien Boutigny <adrien.boutigny@gmail.com>
2021-02-18 22:41:13 +0100

    feat: add header

---
+++ src/components/Container.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :class="['container', `container--${size}`]">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Container',
+  props: {
+    size: {
+      type: String,
+      validator: value => ['large', 'medium'].includes(value),
+      default: 'medium'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.container {
+  margin: 0 auto;
+
+  &--medium {
+    max-width: $container-width-medium;
+  }
+
+  &--large {
+    max-width: $container-width-large;
+  }
+}
+</style>
