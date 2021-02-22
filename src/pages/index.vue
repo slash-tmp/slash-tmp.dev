253d9a0e12e00c83fa070b801a335fb6fca0a0a3
Quentin Bellanger <hello@quentin-bellanger.com>
2021-02-22 22:57:41 +0100

    feat: style quality quote

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,7 @@
   <div class="home-page">
     <div class="home-page__section--dark">
       <HomeHero class="home-page__hero" />
+      <HomeQuality />
     </div>
     <HomeServices class="home-page__services" />
   </div>
@@ -32,6 +33,7 @@ export default {
   &__section {
     &--dark {
       background: $gradient-background-dark;
+      padding: $spacing * 8 0 $spacing * 15;
     }
   }
 
