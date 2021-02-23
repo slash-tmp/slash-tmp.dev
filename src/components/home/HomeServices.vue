01d8368657668b0cd742004c8a79e968307dd84c
Quentin Bellanger <hello@quentin-bellanger.com>
2021-02-23 10:48:36 +0100

    feat: services section (#94)
    * feat: set typography base style
    
    * feat: add services content on homepage
    
    * feat: background text
    
    * feat: opquast link
    
    * fix: set section text color
    
    * fix: adjust line-height and column gap

---
+++ src/components/home/HomeServices.vue
@@ -0,0 +1,99 @@
+<template>
+  <Container class="home-services">
+    <section class="home-services__section">
+      <WebDevIcon class="home-services__icon" />
+      <h2 class="home-services__title">Développement<br />& intégration web</h2>
+      <p>
+        Nous prenons en charge toute la phase de
+        <strong>développement</strong> de votre projet web quelle que soit sa
+        nature : landing page, site vitrine, application web ou site e-commerce.
+        Concrètement, on <strong>transforme</strong> votre cahier des charges et
+        vos maquettes en site web fonctionnel, accessible et utilisable.
+      </p>
+      <p>
+        On travaille de <strong>manière simple</strong> avec un kanban et on
+        intègre les tâches de manière continue pour que nos clients puissent
+        voir évoluer leur projet au fil du développement. On assure également
+        que notre code est bien structuré, documenté, testé et performant pour
+        assurer la <strong>qualité du produit</strong> livré.
+      </p>
+      <span class="home-services__bg-text" aria-hidden="true">inté web</span>
+    </section>
+
+    <section class="home-services__section home-services__section--quality">
+      <WebDevIcon class="home-services__icon" />
+      <h2 class="home-services__title">
+        Audits & conseils<br />
+        en qualité web
+      </h2>
+      <p>
+        Nous sommes <strong>certifiés</strong>
+        <a href="https://www.opquast.com">Opquast</a> et nous avons une grande
+        sensibilité pour la qualité web et l’expérience utilisateur.
+      </p>
+      <p>
+        Nous pouvons vous proposer un audit de qualité web et vous aider à
+        repérer les points d’amélioration sur votre projet.
+      </p>
+      <p>
+        Nous pouvons aussi former votre équipe projet de développement pour une
+        montée en compétence.
+      </p>
+      <span class="home-services__bg-text" aria-hidden="true">conseils</span>
+    </section>
+  </Container>
+</template>
+
+<script>
+export default {
+  name: 'HomeServices'
+}
+</script>
+
+<style lang="scss">
+.home-services {
+  color: $color-text-dark;
+  padding: 0 $spacing * 2;
+
+  &__section {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: $spacing * 4 $spacing * 5;
+    position: relative;
+
+    & + & {
+      margin-top: $spacing * 22;
+    }
+
+    &--quality {
+      grid-template-columns: repeat(3, 1fr);
+    }
+  }
+
+  &__icon {
+    grid-row: 1;
+  }
+
+  &__title {
+    grid-column: 1 / span 2;
+    grid-row: 2;
+  }
+
+  p {
+    line-height: 1.9;
+    grid-row: 3;
+  }
+
+  &__bg-text {
+    font-family: $font-family-sans-serif;
+    font-size: $font-size-normal * 8;
+    font-weight: $font-weight-bold;
+    left: 0;
+    opacity: $opacity-background-text;
+    position: absolute;
+    text-transform: uppercase;
+    top: 75%;
+    z-index: -1;
+  }
+}
+</style>
---
+++ src/components/icons/WebDevIcon.vue
@@ -0,0 +1,30 @@
+<template>
+  <svg width="80" height="80" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M77.071 77.071A10 10 0 0170 80H10A10 10 0 010 70V10A10 10 0 0110 0h60a10 10 0 0110 10v60a10 10 0 01-2.929 7.071zM43.333 6.667H10A3.333 3.333 0 006.667 10v16.667h66.666V10A3.333 3.333 0 0070 6.667H43.333zm-35.69 65.69a3.333 3.333 0 002.357.976h60A3.333 3.333 0 0073.333 70V33.333H6.667V70c0 .884.35 1.732.976 2.357zM20 16.667a3.333 3.333 0 11-6.667 0 3.333 3.333 0 016.667 0zM30 20a3.333 3.333 0 100-6.667A3.333 3.333 0 0030 20zm-4.308 20.975a3.336 3.336 0 00-4.717 4.717l7.642 7.641-7.642 7.642a3.336 3.336 0 004.717 4.717l10-10a3.334 3.334 0 000-4.717l-10-10zM43.333 60h13.334a3.333 3.333 0 010 6.667H43.333a3.333 3.333 0 010-6.667z"
+      fill="url(#web-dev-gradient)"
+    />
+    <defs>
+      <linearGradient
+        id="web-dev-gradient"
+        x1="-.396"
+        y1="32.828"
+        x2="81.115"
+        y2="32.828"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#FFDF96" />
+        <stop offset=".49" stop-color="#FFA99F" stop-opacity=".8" />
+        <stop offset="1" stop-color="#FF719A" />
+      </linearGradient>
+    </defs>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'WebDevIcon'
+}
+</script>
