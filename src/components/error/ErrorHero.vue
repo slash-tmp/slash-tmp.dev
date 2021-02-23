a9e64d579eab207fb3a791eef0adde80b7e19b00
Quentin Bellanger <hello@quentin-bellanger.com>
2021-02-23 12:37:50 +0100

    feat: error page hero

---
+++ src/components/error/ErrorHero.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="error-hero">
+    <Container>
+      <h1>{{ title }}</h1>
+    </Container>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ErrorHero',
+  props: {
+    title: {
+      type: String,
+      default: 'Page introuvable'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.error-hero {
+  background: $gradient-background-dark;
+  color: $color-text-light;
+  padding: $spacing * 8 $spacing * 2 $spacing * 15;
+}
+</style>
