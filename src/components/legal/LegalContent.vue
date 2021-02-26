71a20a431f1f93618f6d5d126423607cbb902056
Quentin Bellanger <hello@quentin-bellanger.com>
2021-02-26 22:14:25 +0100

    feat: display legal page content

---
+++ src/components/legal/LegalContent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="legal-content">
+    <Container>
+      <Container size="small" alignment="left">
+        <NuxtContent class="legal-content__content" :document="content" />
+      </Container>
+    </Container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LegalContent',
+  props: {
+    content: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.legal-content {
+  padding: $spacing * 10 $spacing * 2;
+
+  &__content {
+    --outline-color: #{$color-text-dark};
+  }
+}
+</style>
